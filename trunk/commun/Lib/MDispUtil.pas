8add4d40ba711b6c323ab9102df8948e18454e03
l.santucci <l.santucci@lse.fr>
2018-03-22 11:28:28 +0100

    préparation version 10.000.000.153

--- trunk/commun/Lib/MDispUtil.pas
+++ trunk/commun/Lib/MDispUtil.pas
@@ -432,7 +432,7 @@ begin
   Copyright := '© Copyright ' + Apalatys;
   V_PGI.NumVersion := '10.0' ;
   V_PGI.NumBuild := IntToStr(V_PGI.NumVersionBase) + '.152';
-  V_PGI.DateVersion := EncodeDate(2018, 03, 02) ;
+  V_PGI.DateVersion := EncodeDate(2018, 03, 21) ;
 
   V_PGI.LaSerie := S5;
 
