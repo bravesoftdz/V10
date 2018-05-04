8f2edd41bee5fab07eaa8e203d9361e0aabf3873
Joël TRIFILIEFF <j.trifilieff@lse.fr>
2018-05-04 10:15:05 +0200

    RGPD - Changement nom source

--- trunk/RGPD/BRGPDUtils.pas
+++ trunk/RGPD/BRGPDUtils.pas
@@ -1,4 +1,4 @@
-unit BRGDPDUtils;
+unit BRGPDUtils;
 
 interface
 
@@ -7,7 +7,7 @@ uses
   ;
 
 Type
-  T_RGPDPopulation = (rgpdpNone, rgpdpThird, rgpdpUser, rgpdpResource, rgpdpSuspect);
+  T_RGPDPopulation = (rgpdpNone, rgpdpThird, rgpdpUser, rgpdpResource, rgpdpSuspect, rgpdpContact);
   T_RGPDActions = (rgpdaNone, rgpdaDataExport, rgpdaAnonymization, rgdpaDataRectification, rgdpaConsentRequest, rgdpaConsentResponse);
   T_RGPDTypePath = (rgpdtIncomingRequest, rgpdtTemplateConsent, rgpdtGenerationConsent);
 
@@ -31,6 +31,7 @@ Const
   RGPDUser     = 'USR';
   RGPDResource = 'RES';
   RGPDSuspect  = 'SUS';
+  RGPDContact  = 'CON';
   RGPDCodeJnal = 'RGP';
 
 implementation
@@ -95,6 +96,7 @@ begin
     rgpdpUser     : Result := RGPDUser;
     rgpdpResource : Result := RGPDResource;
     rgpdpSuspect  : Result := RGPDSuspect;
+    rgpdpContact  : Result := RGPDContact;
   else
     Result := '';
   end;
@@ -102,11 +104,12 @@ end;
 
 class function RGPDUtils.GetPopulationFromCode( Code: string): T_RGPDPopulation;
 begin
-  case CaseFromString(Code, [RGPDThird, RGPDUser, RGPDResource, RGPDSuspect]) of
+  case CaseFromString(Code, [RGPDThird, RGPDUser, RGPDResource, RGPDSuspect, rgpdContact]) of
     {THI} 0 : Result := rgpdpThird;
     {USE} 1 : Result := rgpdpUser;
     {RES} 2 : Result := rgpdpResource;
     {SUS} 3 : Result := rgpdpSuspect;
+    {CON} 4 : Result := rgpdpContact;
   else
     Result := rgpdpNone;
   end;
@@ -119,6 +122,7 @@ begin
     rgpdpUser     : Result := TraduireMemoire('Utilisateur');
     rgpdpResource : Result := TraduireMemoire('Ressource');
     rgpdpSuspect  : Result := TraduireMemoire('Suspect');
+    rgpdpContact  : Result := TraduireMemoire('Contact');
   else ;
     Result := '';
   end;
@@ -131,6 +135,7 @@ begin
     rgpdpUser     : Result := TraduireMemoire('utilisateur(s)');
     rgpdpResource : Result := TraduireMemoire('ressource(s)');
     rgpdpSuspect  : Result := TraduireMemoire('suspect(s)');
+    rgpdpContact  : Result := TraduireMemoire('contact(s)');
   else ;
     Result := '';
   end;
@@ -143,6 +148,7 @@ begin
     rgpdpUser     : Result := 'UTILISAT';
     rgpdpResource : Result := 'RESSOURCE';
     rgpdpSuspect  : Result := 'SUSPECTS';
+    rgpdpContact  : Result := 'CONTACT';
   else ;
     Result := '';
   end;
@@ -156,20 +162,6 @@ end;
 class function RGPDUtils.GetSqlTablesException : string;
 var
   TablesName : string;
-
-  function GetComplInf(PSoc, TableName : string) : string;
-  begin
-    Result := '';
-    if not RGPDUtils.AdvancedSettingsEnabled then
-    begin
-
-    end else
-    begin
-    if Pos(TableName, TablesName) = 0 then
-      Result := iif(not GetParamSocSecur(PSoc, False), ', "' + TableName + '"', '');
-    end
-  end;
-
 begin
   if (not RGPDUtils.AdvancedSettingsEnabled) then
     TablesName := ', "TIERSCOMPL", "PROSPECTS", "RTINFOS006", "SUSPECTSCOMPL"'
@@ -191,11 +183,11 @@ end;
 class function RGPDUtils.CanAnonymizableField(TableName, FieldName : string) : boolean;
 begin
   case CaseFromString(TableName, ['TIERS', 'RESSOURCE', 'UTILISAT', 'ADRESSES', 'CONTACT', 'RIB', 'TIERSCOMPL', 'PROSPECTS', 'RTINFOS006', 'SUSPECTS', 'SUSPECTSCOMPL']) of
-    {TIERS}         0  : Result := Pos(';' + FieldName + ';', ';T_AUXILIAIRE;T_TIERS;T_NATUREAUXI;') = 0;
+    {TIERS}         0  : Result := Pos(';' + FieldName + ';', ';T_AUXILIAIRE;T_TIERS;T_COLLECTIF;T_NATUREAUXI;') = 0;
     {RESSOURCE}     1  : Result := Pos(';' + FieldName + ';', ';ARS_RESSOURCE;ARS_AUXILIAIRE;ARS_TYPERESSOURCE;') = 0;
     {UTILISAT}      2  : Result := Pos(';' + FieldName + ';', ';US_UTILISATEUR;') = 0;
     {ADRESSES}      3  : Result := Pos(';' + FieldName + ';', ';ADR_TYPEADRESSE;ADR_NUMEROADRESSE;ADR_NATUREAUXI;ADR_REFCODE;') = 0;
-    {CONTACT}       4  : Result := Pos(';' + FieldName + ';', ';C_TYPECONTACT;C_AUXILIAIRE;C_NUMEROCONTACT;C_TIERS;C_PRINCIPAL;C_TYPECONTACT') = 0;
+    {CONTACT}       4  : Result := Pos(';' + FieldName + ';', ';C_TYPECONTACT;C_AUXILIAIRE;C_NUMEROCONTACT;C_TIERS;C_PRINCIPAL;C_TYPECONTACT;') = 0;
     {RIB}           5  : Result := Pos(';' + FieldName + ';', ';R_AUXILIAIRE;R_NUMERORIB;R_PRINCIPAL;') = 0;
     {TIERSCOMPL}    6  : Result := Pos(';' + FieldName + ';', ';YTC_AUXILIAIRE;YTC_TIERS;YTC_TIERSLIVRE;') = 0;
     {PROSPECTS}     7  : Result := Pos(';' + FieldName + ';', ';RPR_AUXILIAIRE;') = 0;
